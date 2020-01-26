9f9d1ba23789cfb88ef22a809a79d27e68a95cdd
Douglas <dhidekim@gmail.com>
2020-01-26 19:48:54 -0300

    Quando método não retorna nada :void

--- app/ts/models/Negociacoes.ts
+++ app/ts/models/Negociacoes.ts
@@ -1,7 +1,7 @@
 class Negociacoes{
     private _negociacoes: Negociacao[] = [];
 
-    adiciona(negociacao: Negociacao){
+    adiciona(negociacao: Negociacao):void{
 
         this._negociacoes.push(negociacao);
 
