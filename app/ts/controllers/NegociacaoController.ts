fc6a56b40e35b5e0774c49ca98e6d8ddab7bb21d
Douglas <dhidekim@gmail.com>
2020-01-26 19:38:28 -0300

    Forma simplificado para declarar instanciando Objeto.

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -2,7 +2,7 @@ class NegociacaoController{
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
-    private _negociacoes: Negociacoes = new Negociacoes();
+    private _negociacoes = new Negociacoes();
 
     constructor(){
         this._inputData = <HTMLInputElement>document.querySelector('#data');
